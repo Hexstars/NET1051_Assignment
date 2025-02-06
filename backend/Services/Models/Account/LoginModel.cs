b6c07ae4061ff7bdba3936a263041387b23ec6ce
Hexstars <baotnc19@gmail.com>
2025-02-06 18:54:26 +0700

    Chỉnh sửa class LoginModel/RegisterModel

--- backend/Services/Models/Account/LoginModel.cs
+++ backend/Services/Models/Account/LoginModel.cs
@@ -9,8 +9,14 @@ namespace Services.Models.Account
 {
     public class LoginModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
--- backend/Services/Models/Account/RegisterModel.cs
+++ backend/Services/Models/Account/RegisterModel.cs
@@ -9,9 +9,20 @@ namespace Services.Models.Account
 {
     public class RegisterModel
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+
+        [Required]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
