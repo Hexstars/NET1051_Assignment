1a3619285cff9a9d4fa7f71ed153e8020e1dcbe9
Akito0310 <bachubucha@gmail.com>
2025-02-06 19:19:16 +0700

    ICategoryService

---
+++ backend/Services/Contracts/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetCategories();
+        Task<Category> GetCategory(Guid id);
+        Task AddCategory(Category category);
+        Task UpdateCategory(Category category);
+        Task DeleteCategory(Guid id);
+        bool CategoryExists(Guid id);
+    }
+}
